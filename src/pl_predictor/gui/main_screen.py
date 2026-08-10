bfe522203447c76a6c451751211f02dc079a74f7
Jamie Whitehair <jamieabw@protonmail.com>
2026-08-10 13:05:07 +0100

    Added cancellation feature.

--- src/pl_predictor/gui/main_screen.py
+++ src/pl_predictor/gui/main_screen.py
@@ -16,11 +16,19 @@ class MainScreen(Frame):
         self.controller = controller
         self.progress_queue = queue.Queue()
         self.team_selection_frame = TeamSelectionFrame(self)
+        self.sim_label = Label(self)
         self.team_selection_frame.grid(row=0, column=0, rowspan=3)
         self.condition_selection_frame = ConditionSelectionFrame(self)
         self.condition_selection_frame.grid(row=0, column=1)
         self.sim_button = Button(self, text="Simulate", command=lambda :self.start_simulation() )
         self.sim_button.grid(row=3, column=3)
+        self.sim_cancel_button = Button(self, text="Cancel Simulation", command=lambda : self.cancel_simulation(), state="disabled")
+        self.sim_cancel_button.grid(row=3, column=4)
+        self.cancel_event = threading.Event()
+
+    def cancel_simulation(self):
+        if self.sim_thread.is_alive():
+            self.cancel_event.set()
 
 
     def check_simulation(self, league_teams: list[str], champion: str, relegated_teams: list[str], max_sims: int, min_champ_points: int) -> bool:
@@ -40,16 +48,17 @@ class MainScreen(Frame):
 
     def start_simulation(self):
         league_teams, champion, relegated_teams, max_sims, min_champ_points = self.get_simulation_conditions()
-        self.sim_label = Label(self)
+        self.cancel_event.clear()
+        self.sim_cancel_button.config(state="normal")
         self.sim_label.grid(row=4, column=3)
         if self.check_simulation(league_teams, champion, relegated_teams, max_sims, min_champ_points):
             messagebox.showerror(title="Simulation Error", message="The conditions selected are not compatible with the teams selected. Please change conditions or team selection.")
             return
         self.sim_button.config(state="disabled")
-        sim_thread = threading.Thread(target=self.run_simulations, args=(league_teams, champion, relegated_teams, max_sims, min_champ_points), daemon=True)
+        self.sim_thread = threading.Thread(target=self.run_simulations, args=(league_teams, champion, relegated_teams, max_sims, min_champ_points), daemon=True)
         self.sim_label.config(text="Starting Simulation...")
-        sim_thread.start()
-        self.after(10, self.check_progress)
+        self.sim_thread.start()
+        self.after(50, self.check_progress)
         #table = self.run_simulations(champion, relegated_teams, max_sims, min_champ_points, team_ratings)
         #self.controller.show_frame(ResultsScreen)
         #self.controller.frame.display_teams(table)
@@ -65,6 +74,10 @@ class MainScreen(Frame):
                 self.controller.show_frame(ResultsScreen)
                 self.controller.frame.display_teams(data)
                 return
+            elif message_title == "cancelled":
+                self.sim_button.config(state="normal")
+                self.sim_cancel_button.config(state="disabled")
+                self.sim_label.config(text="Simulation Cancelled.")
         except queue.Empty:
             pass
         self.after(10, self.check_progress)
@@ -87,11 +100,12 @@ class MainScreen(Frame):
         for team in league_teams:
                     team_ratings[team] = Team(team, get_team_rating(team))
         for i in range(max_sims):
+            
             if i % 5 == 0:
                 self.progress_queue.put(("progress", i+1))
             table, match_results = simulate_season(team_ratings)
             if (champion == "Any" or champion == table[0].get_name()) and table[0].get_points() >= min_champ_points:
-                print(f"champion: {table[0].get_name()}")
+                #print(f"champion: {table[0].get_name()}")
                 # this is probably not the best way to do this, temporarily here.
                 flag = True
                 for j in range(len(relegated_teams)):
@@ -99,6 +113,9 @@ class MainScreen(Frame):
                         flag = False
                 if flag:
                     break
+            if self.cancel_event.is_set():
+                self.progress_queue.put(("cancelled", None))
+                return
             #print(f"champion: {table[0].get_name()}")
             #print((t.get_name(), t.points) for t in table)
             for team in table:
--- src/pl_predictor/main.py
+++ src/pl_predictor/main.py
@@ -1,5 +1,9 @@
 from pl_predictor.gui.app import App
+import sys
 
+sys.setswitchinterval(0.001)
+
+# TODO: change to multiprocessing rather than multithreading to solve issue with GIL
 
 def main():
     gui = App()
